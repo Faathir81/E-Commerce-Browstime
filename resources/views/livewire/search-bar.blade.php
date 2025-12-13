2d08d74003bd8025271772c85f21ff6c8b3f935d
Faathir81 <4522210033@univpancasila.ac.id>
2025-12-13 20:56:33 +0700

    ganti icon

--- resources/views/livewire/search-bar.blade.php
+++ resources/views/livewire/search-bar.blade.php
@@ -27,7 +27,10 @@
             <button type="button"
                     wire:click="clearSearch"
                     class="absolute right-2 inline-flex items-center justify-center w-8 h-8 rounded-full text-[#3a2a22] transition bg-transparent hover:bg-[#c19a6b] hover:text-white">
-                バ
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11.9921 3.99756L3.99731 11.9924" stroke="#3E2723" stroke-width="1.33247" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M3.99731 3.99756L11.9921 11.9924" stroke="#3E2723" stroke-width="1.33247" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
             </button>
         @endif
     </label>
